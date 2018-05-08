511e59676812a17adc077a86278b4a5d6b15f5e5
Aleksandr <alexlukovkin@gmail.com>
2018-05-08 19:32:15 +0300

    master Add JpaConfiguration

---
+++ src/main/java/com/polytechnic/climbing/configs/JpaConfiguration.java
@@ -0,0 +1,21 @@
+package com.polytechnic.climbing.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+
+@EnableJpaRepositories(basePackages = "com.polytechnic.climbing.repositories")
+@EnableTransactionManagement(proxyTargetClass = true)
+@Configuration
+public class JpaConfiguration {
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+
+}
