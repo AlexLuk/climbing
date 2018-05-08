0f4c7f680d6f11ab3eda47550967fd90e8619ea2
Aleksandr <alexlukovkin@gmail.com>
2018-05-08 19:20:49 +0300

    Initial commit

---
+++ src/main/java/com/polytexnik/climbing/ClimbingApplication.java
@@ -0,0 +1,12 @@
+package com.polytexnik.climbing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimbingApplication.class, args);
+	}
+}
