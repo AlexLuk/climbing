04ed776e45e56ca5d79700cf3988143d6ea69d14
Aleksandr <alexlukovkin@gmail.com>
2018-05-08 19:32:00 +0300

    master Rename package to polytechnic

--- src/main/java/com/polytechnic/climbing/ClimbingApplication.java
+++ src/main/java/com/polytechnic/climbing/ClimbingApplication.java
@@ -1,4 +1,4 @@
-package com.polytexnik.climbing;
+package com.polytechnic.climbing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/polytechnic/climbing/ClimbingApplicationTests.java
+++ src/test/java/com/polytechnic/climbing/ClimbingApplicationTests.java
@@ -1,4 +1,4 @@
-package com.polytexnik.climbing;
+package com.polytechnic.climbing;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
