761f7009b16b77987df54aa1187c6766f33d33b1
Aleksandr <alexlukovkin@gmail.com>
2018-05-31 17:15:58 +0300

    master Add AreaController

---
+++ src/main/java/com/polytechnic/climbing/controllers/AreaController.java
@@ -0,0 +1,29 @@
+package com.polytechnic.climbing.controllers;
+
+import com.polytechnic.climbing.models.area.AreaDto;
+import com.polytechnic.climbing.models.area.AreaForm;
+import com.polytechnic.climbing.services.area.AreaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+public class AreaController {
+
+    private final AreaService areaService;
+
+    @Autowired
+    public AreaController(AreaService areaService) {
+        this.areaService = areaService;
+    }
+
+    @GetMapping("/area/{areaId}")
+    public AreaDto getAreaById(@PathVariable Long areaId) {
+        return areaService.getById(areaId);
+    }
+
+    @PostMapping("/area")
+    public Long addArea(@RequestBody AreaForm areaForm) {
+        return areaService.create(areaForm);
+    }
+}
